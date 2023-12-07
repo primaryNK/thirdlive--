1742c8d061b967b06fcae0210b72efc224d6388b
primaryNK <146739619+primaryNK@users.noreply.github.com>
2023-12-07 14:09:29 +0900

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,3 +1,6 @@
 document.getElementById('goBackButton').addEventListener('click', function() {
   window.location.href = 'index.html'; // 메인 페이지로 이동
 });
+document.getElementById('logo').addEventListener('click', function() {
+  window.location.href = 'index.html'; // 메인 페이지로 이동
+});
