d91b04600e9503905b9186b4fe9c73d722bd38e7
크느는크 <146739619+primaryNK@users.noreply.github.com>
2023-12-08 16:23:43 +0900

    Delete uploadpage directory

--- uploadpage/upload.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('goBackButton').addEventListener('click', function() {
-  window.location.href = 'index.html'; // 메인 페이지로 이동
-});
-document.getElementById('logo').addEventListener('click', function() {
-  window.location.href = 'index.html'; // 메인 페이지로 이동
-});
